a2537b594542b2f5528b22148721718abcfb95ba
Antoun Sawires <antounsawires@gmail.com>
2018-10-02 10:20:52 -0400

    fixing css loading

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
   <head>
+
+    <link rel="stylesheet" href="nav.css">
+    
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top">
 
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
