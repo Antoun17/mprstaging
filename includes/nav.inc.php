6b8517a2ace2fa916395d081356c4f7eabb32857
Antoun Sawires <antounsawires@gmail.com>
2018-10-02 09:53:38 -0400

    change background color

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top" style="background: #393939;">
 
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
