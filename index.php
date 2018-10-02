94ec9a9fe9b8a9d14f0d21f95bd4f340987f7511
Antoun Sawires <antounsawires@gmail.com>
2018-10-02 09:38:14 -0400

    index changes

--- index.php
+++ index.php
@@ -1,7 +1,214 @@
-<?php 
+<head>
+{{attach_library('makerparktheme/bootstrap') }}
+{{attach_library('makerparktheme/popper')}}
+
+<meta charset="utf-8">
+<meta name="Generator" content="Drupal 8 (https://www.drupal.org)">
+<meta property="og:image"content="/sites/default/files/mprshows/mprlogo.png" />
+<meta name="MobileOptimized" content="width">
+<meta name="HandheldFriendly" content="true">
+<meta name="viewport" content="width=device-width, initial-scale=0.8">
+
+<link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
+
+</head>
+
+<html>
+
+  <div class="livestream" style="width: 100%; height: 100%;">
+  <div class="embed-responsive embed-responsive-16by9"><iframe allowfullscreen="true" class="embed-responsive-item" src="https://livestream.com/accounts/25937168/events/7713617/player?width=640&height=360&enableInfoAndActivity=true&defaultDrawer=&autoPlay=true&mute=false"></iframe></div>
 
 
-echo "Hello World";
+<div class="container">
+<center>  <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input name="cmd" type="hidden" value="_s-xclick"><br>
+  <input name="hosted_button_id" type="hidden" value="UCSBNCDQBKV6Q"><br>
+  <input alt="PayPal - The safer, easier way to pay online!" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" type="image"><br>
+</center>
+</div>
+
+<center>
+<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css"> </link>
+<style type="text/css">
+  #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif;  width:200px; margin-left: 1100px;}
+  /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+     We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
 
 
-?>
+      <div class="container wbackground"id="mc_embed_signup container">
+      <form action="https://nyc.us16.list-manage.com/subscribe/post?u=6338c9a8cce6df046dc5ff501&amp;id=d312a7841c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+          <div id="mc_embed_signup_scroll">
+      	<h2>Subscribe to our mailing list</h2>
+      <div class="mc-field-group">
+      	<label for="mce-EMAIL">Email Address </label>
+      	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+      </div>
+      <div class="mc-field-group">
+      	<label for="mce-FNAME">First Name </label>
+      	<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+      </div>
+      <div class="mc-field-group">
+      	<label for="mce-LNAME">Last Name </label>
+      	<input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+      </div>
+      	<div id="mce-responses" class="clear">
+      		<div class="response" id="mce-error-response" style="display:none"></div>
+      		<div class="response" id="mce-success-response" style="display:none"></div>
+      	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+          <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_6338c9a8cce6df046dc5ff501_d312a7841c" tabindex="-1" value=""></div>
+          <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+          </div>
+      </form>
+      </div>
+      <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+
+      <!--End mc_embed_signup-->
+</center>
+<center>
+<img class="container" src="/sites/default/files/mprphotos/appimage.png" style="height: 175px; width: 200px;">
+</center>
+
+
+<div class="container gry" style="width: 60%; height: 20%; padding-bottom: 25px;">
+
+
+  <h2 class="my-4 text-center text-white container">Listen on the go with the Maker Park Radio App!</h2>
+
+
+
+  <div class="row center container" style="padding-left: 27%;">
+    <center>
+    <a class="column" href="https://itunes.apple.com/us/app/makerpark-radio/id1400059324?mt=8" ><img src="/sites/default/files/mprphotos/appstore.png" style="height: 80px; width: 230px;"/></a>
+    <a class="column" href="#" ><img src="/sites/default/files/mprphotos/android.png" style="height: 80px; width: 230px;"/></a>
+    </center>
+  </div>
+
+</div>
+
+
+
+      <!-- SnapWidget -->
+      <div class="container" style="padding-top: 25px;">
+        <!-- SnapWidget -->
+<script src="https://snapwidget.com/js/snapwidget.js"></script>
+<iframe src="https://snapwidget.com/embed/520535" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:100%; "></iframe>
+      </div>
+
+
+<div class="container gcalendar">
+<center>  <iframe src="https://calendar.google.com/calendar/embed?showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=e13rd7dtkjr7ba0lr6t4178n4s%40group.calendar.google.com&amp;color=%23691426&amp;ctz=America%2FNew_York" style="border-width:0" width="100%" height="700px" frameborder="0" scrolling="no"></iframe> </center>
+</div>
+
+
+<div class="container gry" style="width: 50%; height: 150%;">
+
+  <h1 class="my-4 text-center text-white">Our Sponsors</h1>
+<center>
+      <div class="row text-center text-lg-left">
+
+        <div class="col-lg-4 col-md-4 col-xs-6">
+          <a href="https://www.makerspace.nyc/" target="_blank" class="d-block mb-4 h-65 w-65">
+            <img class="" src="/sites/default/files/mprsponsors/simakerspace.png" alt="">
+          </a>
+        </div>
+        <div class="col-lg-4 col-md-4 col-xs-6">
+          <a href="https://www.flagshipbrewery.nyc/" target="_blank" class="d-block mb-4 h-65 w-65">
+            <img class="" src="/sites/default/files/mprsponsors/flagship.png" alt="">
+          </a>
+        </div>
+        <div class="col-lg-4 col-md-4 col-xs-6">
+          <a href="https://statenislandarts.org/" target="_blank" class="d-block mb-4 h-65 w-65">
+            <img class="" src="/sites/default/files/mprsponsors/sia.png" alt="">
+          </a>
+        </div>
+        <div class="col-lg-4 col-md-4 col-xs-6">
+          <a href="https://projectivitygroup.org/" target="_blank" class="d-block mb-4 h-65 w-65">
+            <img class="" src="/sites/default/files/mprsponsors/projectivity.png" alt="">
+          </a>
+        </div>
+        <div class="col-lg-4 col-md-4 col-xs-6">
+          <a href="http://pesochin.video/" target="_blank" class="d-block mb-4 h-65 w-65">
+            <img class="" src="/sites/default/files/mprsponsors/gp-logo.png" alt="">
+          </a>
+        </div>
+        <div class="col-lg-4 col-md-4 col-xs-6">
+          <a href="https://5050store.com/" target="_blank" class="d-block mb-4 h-65 w-65">
+            <img class="" src="/sites/default/files/mprsponsors/5050.png" alt="">
+          </a>
+        </div>
+        <div class="col-lg-4 col-md-4 col-xs-6">
+          <a href="https://www.tfeesstraw.com/" target="_blank" class="d-block mb-4 h-65 w-65">
+            <img class="" src="/sites/default/files/mprsponsors/tfees.png" alt="">
+          </a>
+        </div>
+        <div class="col-lg-4 col-md-4 col-xs-6">
+          <a href="https://www.empressgreen.com/" target="_blank" class="d-block mb-4 h-65 w-65">
+            <img class="" src="/sites/default/files/mprsponsors/expressgreen.jpeg" alt="">
+          </a>
+        </div>
+        <div class="col-lg-4 col-md-4 col-xs-6">
+          <a href="http://sibeerco.com/" target="_blank" class="d-block mb-4 h-65 w-65">
+            <img class="" src="/sites/default/files/mprsponsors/sibeercompany.png" alt="">
+          </a>
+        </div>
+        <div class="col-lg-4 col-md-4 col-xs-6">
+          <a href="https://www.richmondhood.com/" target="_blank" class="d-block mb-4 h-65 w-65">
+            <img class="" src="/sites/default/files/mprsponsors/RHC_LOGO_FULL.png" alt="">
+          </a>
+        </div>
+        <div class="col-lg-4 col-md-4 col-xs-6">
+          <a href="https://sundogtheatre.org/" target="_blank" class="d-block mb-4 h-65 w-65">
+            <img class="" src="/sites/default/files/mprsponsors/sundoglogo.png" alt="">
+          </a>
+        </div>
+        <h2 class="my-4 text-center text-white container">"This project was funded in part by an NYSCA Encore Grant"</h2>
+      </div>
+    </div>
+
+    <style>
+
+    @media only screen and (max-width: 700px) {
+
+    .container {
+    width: 800px;
+    zoom: 60%;
+    }
+
+        }
+
+
+    .gry {
+
+      background-color: #6F6F70;
+      width: 100%;
+
+    }
+
+    .wbackground {
+
+      background-color: #6F6F70;
+    }
+
+    .profile-name {
+      color: white;
+
+    }
+
+    .caption {
+      color: white;
+    }
+}
+
+
+    </style>
+</center>
+  </div>
+
+<script type="text/javascript" src="jquery.">
+
+</script>
+
+
+
+</html>
