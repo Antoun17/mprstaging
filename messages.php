a38fc6b1472ee1a0b5a9df7278e058558b1825e7
Antoun Sawires <antounsawires@gmail.com>
2018-10-02 22:37:10 -0400

    testing pusher

--- messages.php
+++ messages.php
@@ -11,13 +11,7 @@
     '613118',
     $options
   );
-// Check the receive message
-if(isset($_POST['message']) && !empty($_POST['message'])) {
-  $data = $_POST['message'];
-// Return the received message
-if($pusher->trigger('my-channel', 'my-event', $data)) {
-echo 'success';
-} else {
-echo 'error';
-}
-}
+
+  $data['message'] = 'hello world';
+    $pusher->trigger('my-channel', 'my-event', $data);
+  ?>
