fcf1d82942910627bfce3b80ed259b3ae69096cb
Antoun Sawires <antounsawires@gmail.com>
2018-10-03 12:22:09 -0400

    popup chat

--- twitchchat.php
+++ twitchchat.php
@@ -12,6 +12,7 @@ body {font-family: Arial, Helvetica, sans-serif;}
   background-color: #555;
   color: white;
   padding: 16px 20px;
+  padding-top: 50px;
   border: none;
   cursor: pointer;
   opacity: 0.8;
@@ -31,7 +32,7 @@ body {font-family: Arial, Helvetica, sans-serif;}
   right: 15px;
   border: 3px solid #f1f1f1;
   z-index: 9;
-  padding-bottom: 150px; 
+  padding-bottom: 150px;
 }
 
 /* Add styles to the form container */
