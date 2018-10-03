a39140245fb9ead187b20b49dc7f0db3ba5e8112
Antoun Sawires <antounsawires@gmail.com>
2018-10-02 22:18:35 -0400

    testing pusher

--- chat.php
+++ chat.php
@@ -2,8 +2,7 @@
 <head>
 	<title>Pusher Test</title>
   <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-
+  <script src="node_modules/jquery/dist/jquery.js"></script>
 
 <style = "text/css">
 .messages_display {height: 300px; overflow: auto;}
