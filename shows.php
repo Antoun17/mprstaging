5b3f9e2094fd1edd5a514f69b3668b392ed6c87d
Antoun Sawires <antounsawires@gmail.com>
2018-10-10 19:31:24 -0400

    connected :)

--- shows.php
+++ shows.php
@@ -2,7 +2,6 @@
 <html lang="en" dir="ltr">
 <head>
   <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
-  <script src="node_modules/jquery/dist/jquery.js"></script>
 
     <meta charset="utf-8">
     <meta name="Generator" content="Drupal 8 (https://www.drupal.org)">
@@ -335,6 +334,7 @@
         } /* mq 60em*/
       </style>
     </html>
-
+    
+    <script src="node_modules/jquery/dist/jquery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
