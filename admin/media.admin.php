fec9de99dbb2738a098ce6d96d911016197a4168
Antoun Sawires <antounsawires@gmail.com>
2018-10-12 14:03:55 -0400

    added media assign form

--- admin/media.admin.php
+++ admin/media.admin.php
@@ -90,7 +90,7 @@ $result = mysqli_query($nconn, $sqlep); ?>
      <label>Show Name</label>
      <select name="task">
      <?php foreach ($result as $row):?>
-     <option value="<?php echo $row['show_id'] . "|" . $row['show_name'];?>"><?php echo $row['media_url'];?></option>
+     <option value="<?php echo $row['media_url'];?>"><?php echo $row['media_url'];?></option>
      <?php endforeach;?>
      </select>
    </div>
