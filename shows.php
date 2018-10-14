ff976ebed9576ea8555f3b742ed46c22ce2d3f7f
Antoun Sawires <antounsawires@gmail.com>
2018-10-14 01:41:47 -0400

    shows page testing

--- shows.php
+++ shows.php
@@ -121,10 +121,7 @@
 
              <div class="card">
                <a href="shows.php?show_id=<?php echo $row['show_id']; ?>">
-                 <figure class="thumbnail">
-                 <img src="<?php echo $row['show_img']; ?>" alt="meow">
-                 </figure>
-                 <div class="card-content">
+                <div class="card-content">
                    <h2> <a href="shows.php?show_id=<?php echo $row['show_id']; ?>"><?php echo $row['show_name']; ?></a></h2>
                    <p class="card-text collapse" id="viewdetails<?php echo $row['show_id']; ?>"><?php echo $row['show_desc']; ?></p>
                    <p><a class="btn btn-warning " data-toggle="collapse" data-target="#viewdetails<?php echo $row['show_id']; ?>">Show Info</a> <a href="shows.php?show_id=<?php echo $row['show_id']; ?>" class="btn btn-warning">Archives</a> </p>
