aa635b77a1af8dca5134f367a586e0bbec8a20c9
Antoun Sawires <antounsawires@gmail.com>
2018-10-14 13:25:33 -0400

    testing something amazing

--- admin/livestream.admin.php
+++ admin/livestream.admin.php
@@ -43,11 +43,10 @@
       <label>Video Select</label>
       <select name="episodeurl" id="episodeurl" onchange="setStreamSource()">
       <?php foreach ($result as $row):?>
-      <option><?php echo 'https://livestream.com/accounts/25937168/events/7713617/videos/' . $row['episode_url'];?></option>
+      <option><?php echo 'https://livestream.com/accounts/25937168/events/7713617/videos/' . $row['episode_url'] . '/player?width=640&height=360&enableInfoAndActivity=true&defaultDrawer=&autoPlay=true&mute=false';?></option>
       <?php endforeach;?>
       </select>
     </div>
-
 
 
 
