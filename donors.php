c0fd4f80866d4fa3812f32c9f05dc4b193ee9186
Antoun Sawires <antounsawires@gmail.com>
2018-10-18 12:20:06 -0400

    fix

--- donors.php
+++ donors.php
@@ -2,9 +2,16 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Donors</title>
+
+    <?php include("includes/nav.inc.php"); ?>
+
   </head>
   <body style="background-color: #161616;">
-
+    <center>
+    <img style="padding-top: 75px;"class="container" src="/sites/default/files/mprphotos/Donors.jpeg" alt="">
+    </center>
+      </body>
+    </html>
   </body>
 </html>
