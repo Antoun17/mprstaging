b1e916d6a22dd44f291f8504f6a41b18ed0b8df1
Antoun Sawires <antounsawires@gmail.com>
2018-10-25 09:42:08 -0400

    testing

--- index.php
+++ index.php
@@ -44,8 +44,33 @@ $sql = "SELECT * FROM `mpr_live` ORDER BY `live_id` DESC LIMIT 1";
       <?php $row['live_url'];?>
       <?php endforeach;?>
 
+<script>
+function openPage(pageName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
 
-      <button class="tablink" onclick="openPage('Home', this, 'red')" id="defaultOpen">Home</button>
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+</script>
+
+  <button class="tablink" onclick="openPage('Home', this, 'red')" id="defaultOpen">Home</button>
 <button class="tablink" onclick="openPage('News', this, 'green')">News</button>
 <button class="tablink" onclick="openPage('Contact', this, 'blue')">Contact</button>
 <button class="tablink" onclick="openPage('About', this, 'orange')">About</button>
