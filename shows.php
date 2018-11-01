693253d10e54377dc1f3958656f11ab32ca1fa35
Antoun Sawires <antounsawires@gmail.com>
2018-11-01 12:30:39 -0400

    fixing shows

--- shows.php
+++ shows.php
@@ -39,9 +39,9 @@
 
 
              <?php foreach ($result as $row):?>
-               <div class="container">
+
                    <div class="row">
-               <div class="col-sm-2">
+                     <div class="col-sm-2">
                      <a href="shows.php?show_id=<?php echo $row['show_id']; ?>"> </a>
                        <div class="card">
                            <img src="<?php echo $row['show_img']; ?>" class="card-img-top img-adjusted">
@@ -50,10 +50,9 @@
                              <p class="card-text collapse" id="viewdetails<?php echo $row['show_id']; ?>"><?php echo $row['show_desc']; ?></p>
                              <p><a class="btn btn-warning " data-toggle="collapse" data-target="#viewdetails<?php echo $row['show_id']; ?>">Show Info</a> <a href="shows.php?show_id=<?php echo $row['show_id']; ?>" class="btn btn-warning">Archives</a> </p>
                            </div>
-                       </div
-               </div>
-           </div>
-       </div>
+                       </div>
+                     </div>
+                   </div>
 
              <!-- .card -->
            <?php endforeach;
